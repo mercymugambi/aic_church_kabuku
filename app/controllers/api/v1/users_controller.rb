f2f549e180d9d23a1057507163a7b7f31f85639e
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-10-09 23:47:01 +0300

    Create users endpoint

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,8 @@
+# app/controllers/api/v1/users_controller.rb
+class Api::V1::UsersController < ApplicationController
+    def index
+      @users = User.all
+      render json: @users
+    end
+  end
+  
