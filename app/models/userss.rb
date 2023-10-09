89a4366246012b2aa0a79cb0e9360942ee167062
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-10-09 16:54:22 +0300

    Config routes

---
+++ app/models/userss.rb
@@ -0,0 +1,9 @@
+class Userss < ApplicationRecord
+  # belongs_to :role
+
+  # validates :firstname, presence: true
+  # validates :lastname, presence: true
+  # validates :email, presence: true, uniqueness: true
+  # validates :password_digest, presence: true
+#   validates :role_id, presence: true
+end
