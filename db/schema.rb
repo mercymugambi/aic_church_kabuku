a163dbf36748a82fc7d553101de9244d0c8f7624
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-10-10 18:17:47 +0300

    Run Leadership Position Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_142321) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_151117) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_142321) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_devotions_on_user_id"
+  end
+
+  create_table "leadership_positions", force: :cascade do |t|
+    t.string "position_name"
+    t.text "description"
+    t.string "position_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
