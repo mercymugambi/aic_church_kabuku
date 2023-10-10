fd95ba92d1b9d4fde2bdca6e52666477e448ade7
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-10-10 19:16:27 +0300

    Update schema through db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_151117) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_161136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_151117) do
     t.string "position_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_leadership_positions_on_user_id"
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "first_name"
+    t.string "middle_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.string "email"
+    t.string "fellowship_group"
+    t.boolean "baptised"
+    t.bigint "leadership_position_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["leadership_position_id"], name: "index_members_on_leadership_position_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +67,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_151117) do
   end
 
   add_foreign_key "devotions", "users"
+  add_foreign_key "leadership_positions", "users"
+  add_foreign_key "members", "leadership_positions"
 end
