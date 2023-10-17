fdba9e924e48c1c39996ef45bfe922d3c106edc1
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-10-17 22:26:44 +0300

    Config the routes and remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
   root "home#index"
   resources :home, only: [:index]
   namespace :api do
     namespace :v1 do
-      # Resource route for listing users (index action only)
+      
       resources :users, only: [:index]
       resources :members
       resources :devotions
