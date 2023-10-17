18969592eb2d9866d8c4616dd45ece05e186beb1
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-10-17 22:24:58 +0300

    Update devotion model

--- app/models/devotion.rb
+++ app/models/devotion.rb
@@ -1,4 +1,10 @@
 class Devotion < ApplicationRecord
   
   belongs_to :created_by, class_name: 'User'
+
+  validate :check_created_by
+
+  def check_created_by
+    errors.add(:base, 'Devotion must have a creator') if created_by.nil?
+  end
 end
