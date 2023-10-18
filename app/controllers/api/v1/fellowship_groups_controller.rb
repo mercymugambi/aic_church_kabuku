9484b1c256321b53e20f426b4aa1b6e1ac4c4454
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-10-18 15:48:05 +0300

    Update the controller endpoints

--- app/controllers/api/v1/fellowship_groups_controller.rb
+++ app/controllers/api/v1/fellowship_groups_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::FellowshipGroupsController < ApplicationController
+    def index
+        @fellowship_groups = FellowshipGroup.all
+        render json: @fellowship_groups
+    end
 end
