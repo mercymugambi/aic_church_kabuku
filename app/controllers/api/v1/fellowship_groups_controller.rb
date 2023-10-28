a95cd3ca6c54c8937538aaeee48b526a7e4e7c9e
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-10-28 20:19:54 +0300

    Update with created by to auto pick from the db

--- app/controllers/api/v1/fellowship_groups_controller.rb
+++ app/controllers/api/v1/fellowship_groups_controller.rb
@@ -3,4 +3,21 @@ class Api::V1::FellowshipGroupsController < ApplicationController
     @fellowship_groups = FellowshipGroup.all
     render json: @fellowship_groups
   end
+  def create
+    @fellowship_groups = FellowshipGroup.new(groups_params)
+    @fellowship_groups.created_by = current_user.id
+
+    if @fellowship_groups.save
+      redirect_to @fellowship_groups, notice: 'Fellowship Group was successfully created.'
+    else
+      puts @fellowship_groups.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def groups_params
+    params.require(:fellowship_groups).permit(:group_name, :group_code)
+  end
 end
